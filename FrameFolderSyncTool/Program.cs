813441a03ae35e80e204b8452e7de615c694ddfa
Kalle Launiala <kalle.launiala@protonit.net>
2018-09-26 00:54:07 +0300

    Initial commit with OBS filter and FrameFolderSyncTool

---
+++ FrameFolderSyncTool/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrameFolderSyncTool
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage params: \"Source dir\" \"Target dir\"");
+            }
+
+            try
+            {
+                var sourcePath = args[0];
+                var targetPath = args[1];
+                var sourceDir = new DirectoryInfo(sourcePath);
+                var targetDir = new DirectoryInfo(targetPath);
+                if (!sourceDir.Exists)
+                    throw new ArgumentException($"Source dir does not exist: {sourcePath}");
+                if (!targetDir.Exists)
+                    throw new ArgumentException($"Target dir does not exist: {targetPath}");
+                if (sourceDir.Name != "Captures")
+                    throw new ArgumentException($"Source dir must be named Captures: {sourcePath}");
+                if (targetDir.Name != "Captures")
+                    throw new ArgumentException($"Target dir must be named Captures: {targetPath}");
+                while (true)
+                {
+                    var doneFileList = sourceDir.GetFiles("done", SearchOption.AllDirectories);
+                    var movableDirectories =
+                        doneFileList.Select(file => file.Directory).OrderBy(dir => dir.Name).ToArray();
+                    foreach (var movableDir in movableDirectories)
+                    {
+                        var files = movableDir.GetFiles().OrderBy(file => file.Name).ToArray();
+                        var moveTargetDir = new DirectoryInfo(Path.Combine(targetDir.FullName, movableDir.Name));
+                        if(!moveTargetDir.Exists)
+                            moveTargetDir.Create();
+                        Console.Write($"Copying folder {moveTargetDir.Name}...");
+                        foreach (var file in files)
+                        {
+                            var targetFile = Path.Combine(moveTargetDir.FullName, file.Name);
+                            file.CopyTo(targetFile);
+                            file.Delete();
+                        }
+                        Console.WriteLine(" done.");
+                        movableDir.Delete();
+                    }
+                    await Task.Delay(10000);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error happened, exiting (press enter to exit):");
+                Console.WriteLine(exception.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ PublishedRelease/README.txt
@@ -0,0 +1,30 @@
+OBS Frame Filter and Folder Syncing Toolkit
+-------------------------------------------
+
+OBS filter: repeating-frame-capture-filter.dll (x64)
+
+- Needs to be copied to OBS plugin folder
+(such as "C:\Program Files (x86)\obs-studio\obs-plugins\64bit")
+- Needs to be added as a filter to chosen source
+
+Functionality/Usage:
+When active starts to create images under C:\RepeatingFilter\Captures
+(might be required to create the directories for it, if the OBS running permissions do not suffice for creating them)
+
+
+
+FrameFolderSyncTool.dll (.NET Core Application)
+- Requires .NET Core Runtime 2.1 (Runtime should be enough, no need for SDK)
+https://www.microsoft.com/net/download/dotnet-core/2.1
+
+Functionality/Usage:
+- Using .NET Core Runtime "dotnet" command to run
+- Use normal command line or .bat/.cmd file 
+dotnet FrameFolderSyncTool.dll <source captures folder> <target captures folder>
+
+Example (running from other computer to access the C:\RepeatingFilter\Captures through named source "RepeatingFilter"):
+
+dotnet FrameFolderSyncTool.dll \\SourceComputer\RepeatingFilter\Captures\ C:\What\Ever\Path\That\Ends\With\Captures\
+
+
+When working, should stay open and move all folders that have "done" file with them named accordingly to target location and removing them as moved from the source. Ctrl + C ends the program when running.
